bf59fcf645b63b56e69b7c225a3c46eca12cb9a7
seonghwikyeong <122859906+seonghwikyeong@users.noreply.github.com>
2023-01-30 11:48:28 +0900

    first commit

---
+++ shk.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import datetime
+
+st.set_page_config(
+    page_title = "Streamlit 프로토타입 만들기"
+    page_icon = "",
+    layout='wide'
+)
+
+st.text('성휘경 : Streamlit 프로토타입 만들기')
