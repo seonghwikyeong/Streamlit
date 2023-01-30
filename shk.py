5ef1d1a6d257a8de9f874c35ec889bf70b207bc6
seonghwikyeong <122859906+seonghwikyeong@users.noreply.github.com>
2023-01-30 11:56:54 +0900

    Add files via upload

--- shk.py
+++ shk.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import pandas as pd
-import datetime
 
 st.set_page_config(
-    page_title = "Streamlit 프로토타입 만들기"
-    page_icon = "",
+    page_title = "Streamlit 프로토타입 만들기",
     layout='wide'
 )
 
